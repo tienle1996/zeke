8f37e7a77006b083a819c51a4044880eadad29ca
tienle1996 <47994792+tienle1996@users.noreply.github.com>
2022-04-12 09:44:20 -0500

    make it better

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://zeke-backend.herokuapp.com//api",
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
